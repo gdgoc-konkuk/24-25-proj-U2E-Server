ba0b3148dd274e4ec553cd200c449c0f319a23c5
janghyunjun <buzz03312@naver.com>
2025-05-03 18:22:35 +0900

    [feat] 뉴스를 기준으로 기후 문제를 반환하는 쿼리 (#9)

--- U2E/src/main/java/Konkuk/U2E/domain/news/repository/ClimateRepository.java
+++ U2E/src/main/java/Konkuk/U2E/domain/news/repository/ClimateRepository.java
@@ -1,9 +1,13 @@
 package Konkuk.U2E.domain.news.repository;
 
 import Konkuk.U2E.domain.news.domain.Climate;
+import Konkuk.U2E.domain.news.domain.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClimateRepository extends JpaRepository<Climate, Long> {
+    List<Climate> findClimatesByNews(News news);
 }
