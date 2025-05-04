24d9c6884404535e2c650c702191c724f61ea9a1
janghyunjun <buzz03312@naver.com>
2025-05-05 02:17:42 +0900

    [chore] BaseResponse 수정 (#11)

--- U2E/src/main/java/Konkuk/U2E/global/response/BaseResponse.java
+++ U2E/src/main/java/Konkuk/U2E/global/response/BaseResponse.java
@@ -1,57 +1,44 @@
 package Konkuk.U2E.global.response;
 
+import Konkuk.U2E.global.response.status.BaseExceptionResponseStatus;
 import Konkuk.U2E.global.response.status.ResponseStatus;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static Konkuk.U2E.global.response.status.BaseExceptionResponseStatus.SUCCESS;
+
 @Getter
-@JsonPropertyOrder({"code", "status", "message", "data"})
+@JsonPropertyOrder({"success", "code", "message", "data"})
 public class BaseResponse<T> implements ResponseStatus {
 
+    private final boolean success;
     private final int code;
-    private final HttpStatus status;
     private final String message;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private final T data;
 
-    public BaseResponse(HttpStatus status, String message, T data) {
-        this.code = status.value();
-        this.message = message;
-        this.status = status;
+    public BaseResponse(T data) {
+        this.success = true;
+        this.code = SUCCESS.getCode();
+        this.message = SUCCESS.getMessage();
         this.data = data;
     }
 
-    public static <T> BaseResponse<T> of(HttpStatus status, String message, T data) {
-        return new BaseResponse<>(status, message, data);
-    }
-
-    public static <T> BaseResponse<T> of(HttpStatus status, T data) {
-        return of(status, status.name(), data);
-    }
-
     public static <T> BaseResponse<T> ok(T data) {
-        return of(HttpStatus.OK, data);
+        return new BaseResponse<>(data);
     }
 
-    public static BaseResponse<Void> of(HttpStatus status) {
-        return new BaseResponse<>(status, status.name(), null);
-    }
-
-    public static BaseResponse<Void> ok() {
-        return of(HttpStatus.OK);
+    @Override
+    public boolean getSuccess() {
+        return success;
     }
 
     @Override
     public int getCode() {
         return code;
-    }
-
-    @Override
-    public HttpStatus getStatus() {
-        return status;
     }
 
     @Override
