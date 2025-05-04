44cefc917e731910e52290f277f0aea6829c9b71
janghyunjun <buzz03312@naver.com>
2025-05-05 02:19:34 +0900

    [feat] 최근 뉴스 반환 시에 기후 문제를 Enum 그대로 반환하도록 수정 (#11)

--- U2E/src/main/java/Konkuk/U2E/domain/news/dto/response/LatelyNews.java
+++ U2E/src/main/java/Konkuk/U2E/domain/news/dto/response/LatelyNews.java
@@ -1,19 +1,22 @@
 package Konkuk.U2E.domain.news.dto.response;
 
+import Konkuk.U2E.domain.news.domain.ClimateProblem;
+import Konkuk.U2E.domain.news.domain.News;
+
 import java.util.List;
 
 public record LatelyNews(
         Long newsId,
         List<String> regionList,
-        List<String> climateList,
+        List<ClimateProblem> climateList,
         String newsTitle
 ) {
-    public static LatelyNews of(Long newsId, List<String> regionList, List<String> climateList, String newsTitle) {
+    public static LatelyNews of(List<ClimateProblem> climateList, List<String> regionList, News news) {
         return new LatelyNews(
-                newsId,
+                news.getNewsId(),
                 regionList,
                 climateList,
-                newsTitle
+                news.getNewsTitle()
         );
     }
 }
