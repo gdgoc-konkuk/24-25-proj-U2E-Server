63d728ff1a999c06717ee35669eb1f6523847150
janghyunjun <buzz03312@naver.com>
2025-05-06 16:23:54 +0900

    [feat] NewsPin 못 찾는 예외 핸들링 (#15)

---
+++ U2E/src/main/java/Konkuk/U2E/domain/pin/exception/handler/PinControllerAdvice.java
@@ -0,0 +1,27 @@
+package Konkuk.U2E.domain.pin.exception.handler;
+
+import Konkuk.U2E.domain.news.exception.NewsNotFoundException;
+import Konkuk.U2E.domain.pin.exception.NewsPinNotFoundException;
+import Konkuk.U2E.global.response.BaseErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static Konkuk.U2E.global.response.status.BaseExceptionResponseStatus.NEWSPIN_NOT_FOUND;
+import static Konkuk.U2E.global.response.status.BaseExceptionResponseStatus.NEWS_NOT_FOUND;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
+@RestControllerAdvice(basePackages = "Konkuk.U2E.domain.pin")
+public class PinControllerAdvice {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NewsPinNotFoundException.class)
+    public BaseErrorResponse handle_NewsNotFoundException(NewsPinNotFoundException e) {
+        log.error("[handle_NewsPinNotFoundException]", e);
+        return new BaseErrorResponse(NEWSPIN_NOT_FOUND, e.getMessage());
+    }
+}
