558b1d54585631e92499e55d93378112f9948f15
Jay Park <pjh6183@naver.com>
2025-05-07 23:37:01 +0900

    [feat] newsId로 Comment를 찾는 쿼리

--- U2E/src/main/java/Konkuk/U2E/domain/comment/repository/CommentRepository.java
+++ U2E/src/main/java/Konkuk/U2E/domain/comment/repository/CommentRepository.java
@@ -2,8 +2,14 @@ package Konkuk.U2E.domain.comment.repository;
 
 import Konkuk.U2E.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query("SELECT c FROM Comment c WHERE c.news.newsId = :newsId")
+    List<Comment> findCommentsByNewsId(@Param("newsId") Long newsId);
 }
