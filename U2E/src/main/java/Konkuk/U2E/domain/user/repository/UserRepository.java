618eb747b9b454bc074ba6e888ce1617eaa78541
Jay Park <pjh6183@naver.com>
2025-05-07 21:40:35 +0900

    [feat] name으로 User 객체 찾는 쿼리

--- U2E/src/main/java/Konkuk/U2E/domain/user/repository/UserRepository.java
+++ U2E/src/main/java/Konkuk/U2E/domain/user/repository/UserRepository.java
@@ -2,8 +2,14 @@ package Konkuk.U2E.domain.user.repository;
 
 import Konkuk.U2E.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.name = :name")
+    User findUserByName(@Param("name") String name);
 }
