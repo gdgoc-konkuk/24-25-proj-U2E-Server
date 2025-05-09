34ed3a357f8a095d6d944ed21eb917abd0e0fefb
janghyunjun <buzz03312@naver.com>
2025-05-09 21:26:58 +0900

    [feat] 지역으로 핀 찾는 쿼리 추가 (#16)

--- U2E/src/main/java/Konkuk/U2E/domain/pin/repository/PinRepository.java
+++ U2E/src/main/java/Konkuk/U2E/domain/pin/repository/PinRepository.java
@@ -1,9 +1,11 @@
 package Konkuk.U2E.domain.pin.repository;
 
 import Konkuk.U2E.domain.pin.domain.Pin;
+import Konkuk.U2E.domain.pin.domain.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PinRepository extends JpaRepository<Pin, Long> {
+    Pin findByRegion(Region region);
 }
