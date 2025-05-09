f79bb65ee4ffd7fc457342a179ba0e0ea6fcc782
janghyunjun <buzz03312@naver.com>
2025-05-09 21:27:17 +0900

    [feat] 지역 이름으로 지역 엔티티 찾는 쿼리 (#16)

--- U2E/src/main/java/Konkuk/U2E/domain/pin/repository/RegionRepository.java
+++ U2E/src/main/java/Konkuk/U2E/domain/pin/repository/RegionRepository.java
@@ -1,9 +1,15 @@
 package Konkuk.U2E.domain.pin.repository;
 
+import Konkuk.U2E.domain.pin.domain.Pin;
 import Konkuk.U2E.domain.pin.domain.Region;
+import org.apache.el.stream.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
+
+    Optional<Region> findByName(String region);
 }
