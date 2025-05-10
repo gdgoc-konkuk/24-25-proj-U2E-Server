3297c71eeac8c78e7ba7a351464e961631fcbf83
janghyunjun <buzz03312@naver.com>
2025-05-10 19:50:54 +0900

    [feat] Comment 관련 로그 추가 (#22)

--- U2E/src/main/java/Konkuk/U2E/domain/comment/controller/CommentController.java
+++ U2E/src/main/java/Konkuk/U2E/domain/comment/controller/CommentController.java
@@ -6,8 +6,10 @@ import Konkuk.U2E.domain.comment.service.CommentService;
 import Konkuk.U2E.global.annotation.LoginUser;
 import Konkuk.U2E.global.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/comments")
 @RequiredArgsConstructor
@@ -17,11 +19,13 @@ public class CommentController {
 
     @GetMapping("/{newsId}")
     public BaseResponse<GetCommentsResponse> getComments(@PathVariable("newsId") Long newsId) {
+        log.info("댓글 조회 요청, newsId: {}", newsId);
         return BaseResponse.ok(commentService.getCommentInfo(newsId));
     }
 
     @PostMapping
     public BaseResponse<Void> postComments(@LoginUser String username, @RequestBody PostCommentCreateRequest request) {
+        log.info("댓글 작성 요청, username: {}, request: {}", username, request);
         commentService.createComment(username, request);
         return BaseResponse.ok(null);
     }
