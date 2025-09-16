6212b44dd5c3ccc6cecbde044454f1b03d3b9dcc
Jay Park <pjh6183@naver.com>
2025-09-16 19:18:01 +0900

    [CHORE] 기존 뉴스 상세조회 응답 dto 변경

--- U2E/src/main/java/Konkuk/U2E/domain/news/dto/response/GetNewsInfoResponse.java
+++ U2E/src/main/java/Konkuk/U2E/domain/news/dto/response/GetNewsInfoResponse.java
@@ -3,6 +3,7 @@ package Konkuk.U2E.domain.news.dto.response;
 import Konkuk.U2E.domain.news.domain.ClimateProblem;
 import Konkuk.U2E.domain.news.domain.News;
 import Konkuk.U2E.domain.news.service.mapper.NewsMappingResult;
+import Konkuk.U2E.global.openApi.gemini.dto.response.RelatedArticle;
 
 import java.util.List;
 
@@ -13,10 +14,22 @@ public record GetNewsInfoResponse(
         String newsUrl,
         String newsImageUrl,
         String newsBody,
-        String newsDate
+        String newsDate,
+        String aiSolution,
+        List<RelatedArticle> aiRelated
 ) {
-    public static GetNewsInfoResponse of(NewsMappingResult newsMappingResult) {
+    public static GetNewsInfoResponse of(NewsMappingResult newsMappingResult, String aiSolution, List<RelatedArticle> aiRelated) {
         News news = newsMappingResult.news();
-        return new GetNewsInfoResponse(newsMappingResult.climateProblems(), newsMappingResult.regionNames(), news.getNewsTitle(), news.getNewsUrl(), news.getImageUrl(), news.getNewsBody(), news.getNewsDate().toString());
+        return new GetNewsInfoResponse(
+                newsMappingResult.climateProblems(),
+                newsMappingResult.regionNames(),
+                news.getNewsTitle(),
+                news.getNewsUrl(),
+                news.getImageUrl(),
+                news.getNewsBody(),
+                news.getNewsDate().toString(),
+                aiSolution,
+                aiRelated
+        );
     }
 }
